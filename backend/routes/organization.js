41c5fdf0c7c3b545e51e24fae3fab4ff9ec441a4
misaelh2010 <61474910+misaelh2010@users.noreply.github.com>
2022-10-05 05:05:31 -0500

    added get all,  by organization_id, and post

--- backend/routes/organization.js
+++ backend/routes/organization.js
@@ -17,4 +17,35 @@ router.get("/", (req, res, next) => {
     ).sort({ 'updatedAt': -1 }).limit(10);
 });
 
+//GET single entry by ID
+router.get("/id/:id", (req, res, next) => {
+    organization.find(
+        { organization_id: req.params.id },
+        (error, data) => {
+            if (error) {
+                return next(error);
+            } else {
+                res.json(data);
+            }
+        }
+    );
+});
+
+//POST
+router.post("/", (req, res, next) => {
+    organization.create(
+        req.body,
+        (error, data) => {
+            if (error) {
+                return next(error);
+            } else {
+                res.json(data);
+            }
+        }
+    );
+    organization.createdAt;
+    organization.updatedAt;
+    organization.createdAt instanceof Date;
+});
+
 module.exports = router;
